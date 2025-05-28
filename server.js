69195c4604438c68b00ef78605e2dd1be9ab75ed
so1ove1 <159862767+so1ove1@users.noreply.github.com>
2025-05-28 16:04:47 +0800

    Update server configuration

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const app = express();
 app.use(cors({
     origin: 'https://manager.bratskprofil.ru',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 app.use(express.json());
 
