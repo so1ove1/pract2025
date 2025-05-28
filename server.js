9f71ba62bb3f5ed55efd68b11ec542c86cc23769
so1ove1 <159862767+so1ove1@users.noreply.github.com>
2025-05-29 03:55:08 +0800

    Fix server configuration

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 });
 
 // Initialize database and start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 sequelize.authenticate()
     .then(() => {
