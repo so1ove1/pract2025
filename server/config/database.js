e3bc7fbf93e6ff484ad7e31b536bae5b9a766f8e
so1ove1 <159862767+so1ove1@users.noreply.github.com>
2025-05-28 16:21:33 +0800

    Restore MySQL database configuration

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,8 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const sequelize = new Sequelize(process.env.SUPABASE_DB_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
+const sequelize = new Sequelize('bratskprof_bit', 'bratskprof_bit', 'a*q0@U@[', {
+    host: 'localhost',
+    dialect: 'mysql',
     logging: false,
     timezone: '+05:00',
     define: {
