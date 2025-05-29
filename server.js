d2304b930feb586bd0637c1fbc83f9c4953faf71
so1ove1 <159862767+so1ove1@users.noreply.github.com>
2025-05-29 17:58:35 +0800

    Fix Server Port Binding

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ sequelize.authenticate()
         return sequelize.sync();
     })
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+        app.listen(PORT, '127.0.0.1', () => {
+            console.log(`Server running on http://127.0.0.1:${PORT}`);
         });
     })
     .catch(err => {
